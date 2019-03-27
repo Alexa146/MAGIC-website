85f06e90f69660019de9c596b84af19dcadd5d4e
Alexa146 <45612155+Alexa146@users.noreply.github.com>
2019-03-26 20:14:30 -0700

    I added the demo files

---
+++ magicdemo/MAGICdemo.2.js
@@ -0,0 +1,18 @@
+function secondFunction(){
+  window.location.href = 'MAGICdemohtml1.html'
+}
+function secondFunction2(){
+  window.location.href = 'MAGICdemohtml2.html'
+}
+function secondFunction3(){
+  window.location.href = 'MAGICdemohtml3.html'
+}
+function secondFunction4(){
+  window.location.href = 'MAGICdemohtml4.html'
+}
+function secondFunction5(){
+  window.location.href = 'MAGICdemo.html'
+}
+function secondFunction6(){
+  window.location.href = 'MAGICdemo.5.html'
+}
---
+++ magicdemo/MAGICdemojs.js
@@ -0,0 +1,12 @@
+function concatenate(what, happens) {
+   var full;
+   full = what + happens;
+   return full;
+}
+function secondFunction() {
+   var result;
+   result = concatenate('the jellyfish ', 'tells you that you are being rude and stings you to death');
+   document.write (result );
+}
+projects.onClick = function(){
+  window.location.href = 'MAGICdemoop.1.html'
